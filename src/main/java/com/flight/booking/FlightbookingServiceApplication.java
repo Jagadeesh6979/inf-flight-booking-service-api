991cd3265532948de659ea0ec7f2c2eca041a76b
jagadeesh6979 <jagadeesh6979@gmail.com>
2025-01-26 18:40:37 +0530

    Initial commit

---
+++ src/main/java/com/flight/booking/FlightbookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flight.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightbookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightbookingServiceApplication.class, args);
+	}
+
+}
